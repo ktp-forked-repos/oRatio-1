f640ecd0452f8c0e9ef418594850812d92c8d9b3
Riccardo De Benedictis <riccardo.debenedictis@istc.cnr.it>
2019-02-21 17:24:35 +0100

    minor cleanings..

--- solver/types/state_variable.cpp
+++ solver/types/state_variable.cpp
@@ -333,14 +333,12 @@ std::vector<std::pair<lit, double>> state_variable::sv_flaw::evaluate()
         {
             rational work = (slv.arith_value(a1_end).get_rational() - slv.arith_value(a1_start).get_rational()) * (slv.arith_value(a0_end).get_rational() - slv.arith_value(a1_start).get_rational());
             choices.push_back({a0_before_a1, 1l - 1l / (static_cast<double>(work.numerator()) / work.denominator())});
-            LOG("b" << std::to_string(a0_before_a1.get_var()) << ": σ" << as[0]->get_sigma() << " <= σ" << as[1]->get_sigma() << " (" << std::to_string(choices.back().second) << ")");
         }
         lit a1_before_a0 = sv.leqs.at(as[1]).at(as[0]);
         if (slv.get_sat_core().value(a1_before_a0) != False)
         {
             rational work = (slv.arith_value(a0_end).get_rational() - slv.arith_value(a0_start).get_rational()) * (slv.arith_value(a1_end).get_rational() - slv.arith_value(a0_start).get_rational());
             choices.push_back({a1_before_a0, 1l - 1l / (static_cast<double>(work.numerator()) / work.denominator())});
-            LOG("b" << std::to_string(a1_before_a0.get_var()) << ": σ" << as[1]->get_sigma() << " <= σ" << as[0]->get_sigma() << " (" << std::to_string(choices.back().second) << ")");
         }
 
         expr a0_tau = as[0]->get(TAU);
